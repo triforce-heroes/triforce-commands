0fbee3d36ba8d1f6129410064d2d352ce0daaa96
David Rodrigues <david.proweb@gmail.com>
2024-06-05 11:33:32 -0300

    feat: improve debug entries to include related entries;

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,38 +1,71 @@
-import { inspect } from "node:util";
-
 import { Driver } from "../Driver.js";
 import { EntryCommand } from "../entries/EntryCommand.js";
 import { EntryCommandUnknown } from "../entries/EntryCommandUnknown.js";
+
+interface Sample {
+  raw: string;
+  commandsCounter: number;
+}
 
 export function debugEntries(
   driver: Driver,
   entries: string[],
   length: number,
+  includeSamples = false,
 ) {
   const unknowns = new Set<string>();
-  const knowns = new Set<string>();
+  const knowns = new Map<string, string[]>();
 
   for (const entry of entries) {
     for (const command of driver.parseRaw(entry).entries) {
       if (command instanceof EntryCommandUnknown) {
         unknowns.add(command.command.slice(0, length));
       } else if (command instanceof EntryCommand) {
-        knowns.add(command.command);
+        if (!knowns.has(command.command)) {
+          knowns.set(command.command, []);
+        }
+
+        knowns.get(command.command)!.push(entry);
       }
     }
   }
 
-  process.stdout.write(
-    inspect(
-      {
-        unknowns: [...unknowns].sort(),
-        knowns: [...knowns].sort(),
-      },
-      {
-        depth: null,
-        colors: true,
-        maxArrayLength: null,
-      },
-    ),
-  );
+  const samples = new Map<string, string[]>();
+
+  if (includeSamples) {
+    for (const [knownCommand, knownsSamples] of knowns.entries()) {
+      const knownSamples: Sample[] = [];
+
+      for (const knownSample of knownsSamples) {
+        knownSamples.push({
+          raw: knownSample,
+          commandsCounter: driver
+            .parseRaw(knownSample)
+            .entries.reduce(
+              (count, command) =>
+                count + Number(command instanceof EntryCommand),
+              0,
+            ),
+        });
+      }
+
+      samples.set(
+        knownCommand,
+        knownSamples
+          .sort(
+            (a, b) =>
+              a.commandsCounter - b.commandsCounter ||
+              a.raw.length - b.raw.length,
+          )
+          .map(({ raw }) => raw)
+          .slice(0, 10),
+      );
+    }
+  }
+
+  return {
+    unknowns: [...unknowns].sort(),
+    knowns: [...knowns.keys()].sort(),
+    samples: [...samples.entries()].sort(([a], [b]) => a.localeCompare(b)),
+  };
 }
